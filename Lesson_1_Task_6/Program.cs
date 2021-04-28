ccffd3b448b4b63c9eb79dd97b675b60b0ea154d
Shturmovik <Radist-KPS@Yandex.ru>
2021-04-28 20:56:37 +0300

    Доделка ДЗ

---
+++ Lesson_1_Task_6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1_Task_6
+{
+    #region Task
+    /* 6. *Создать класс с методами, которые могут пригодиться в вашей учебе
+     * Коновалов П.С.
+     */
+    #endregion
+    class Program : Barny
+    {
+        static void Main(string[] args)
+        {
+            SlapFace();
+        }
+    }
+
+    class Barny
+    {
+        public static void SlapFace()
+        {
+            Console.WriteLine("Вам дали ободряющую пощечину =)");
+        }
+
+    }
+}
